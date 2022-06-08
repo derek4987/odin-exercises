c167583c3e5a98fba31448e55be3ad8b62344e89
Derek Nunes <dereknunes28@gmail.com>
2022-06-08 00:21:40 -0500

    form creation and focus styling

---
+++ js-form-validation/js-files.js
@@ -0,0 +1,7 @@
+// js content
+
+const emailInput = document.querySelector('#email');
+const countryInput = document.querySelector('#country');
+const zipCodeInput = document.querySelector('#zipCode');
+const passwordInput = document.querySelector('#password');
+const confirmPasswordInput = document.querySelector('#confirmPassword');
