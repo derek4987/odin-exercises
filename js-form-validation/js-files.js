a0595dd035bfe14166c022c0172c981e04306d23
Derek Nunes <dereknunes28@gmail.com>
2022-06-09 00:44:14 -0500

    start form validation

--- js-form-validation/js-files.js
+++ js-form-validation/js-files.js
@@ -1,7 +1,30 @@
 // js content
 
 const emailInput = document.querySelector('#email');
+const emailError = emailInput.nextElementSibling.nextElementSibling;
 const countryInput = document.querySelector('#country');
+const countryError = countryInput.nextElementSibling.nextElementSibling;
 const zipCodeInput = document.querySelector('#zipCode');
+const zipCodeError = zipCodeInput.nextElementSibling.nextElementSibling;
 const passwordInput = document.querySelector('#password');
+const passwordError = passwordInput.nextElementSibling.nextElementSibling;
 const confirmPasswordInput = document.querySelector('#confirmPassword');
+const confirmPasswordError = confirmPasswordInput.nextElementSibling.nextElementSibling;
+
+//email validation
+emailInput.addEventListener('input', function(e) {
+    if (emailInput.validity.valid) {
+        emailError.textContent = '';
+        emailError.className = 'error';
+    } else {
+        showError()
+    }
+})
+
+function showError() {
+    if(emailInput.validity.valueMissing) {
+        emailError.textContent = 'You need to enter a valid e-mail address.';
+    }
+
+    emailError.className = 'error active';
+}
