6ea0094d6d7f06f71e261e08d58464a9d4cd5f09
Derek Nunes <dereknunes28@gmail.com>
2022-07-06 13:11:09 -0500

    shell form of functions and annotations

--- testing-practice/js-file.js
+++ testing-practice/js-file.js
@@ -1,12 +1,15 @@
 // 1. A capitalize function that takes a string and returns it with the first character capitalized
+function capitalize(string) {
 
+}
 
 // 2. A reverseString function that takes a string and returns it reversed.
+function reverseString(string) {
 
+}
 
 /* 3. A calculator object that contains functions for the basic operations: add, subtract, divide, and multiply. 
 Each of these functions should take two numbers and return the correct calculation. */
-
 function add(a, b) {
     return a + b;
 }
@@ -15,14 +18,32 @@ function subtract(a, b, c) {
     return a - b - c;
 }
 
-// 4. A caesarCipher function that takes a string and returns it with each character “shifted”.
+function multiply(a, b) {
 
+}
+
+function divide(a, b) {
+
+}
+
+// 4. A caesarCipher function that takes a string and returns it with each character “shifted”.
+function caesarCipher(string) {
+
+}
 
 /* 5. An analyzeArray function that takes an array of numbers and returns an object with the following 
 properties: average, min, max, and length. */
+function analyzeArray(array) {
 
+}
 
-export { 
+export {
+        capitalize,
+        reverseString, 
         add,
-        subtract 
+        subtract,
+        multiply,
+        divide,
+        caesarCipher,
+        analyzeArray
     };
--- testing-practice/test.js
+++ testing-practice/test.js
@@ -1,6 +1,24 @@
+import { capitalize } from "./js-file";
+import { reverseString } from "./js-file";
 import { add } from "./js-file";
+import { subtract } from "./js-file";
+import { multiply } from "./js-file";
+import { divide } from "./js-file";
+import { caesarCipher } from "./js-file";
+import { analyzeArray } from "./js-file";
+
+// 1. capitalize
 
 
+// 2. reverseString
+
+
+// 3. calculator: add, subtract, multiply, divide
 test('adds 1 + 2 to equal 3', () => {
   expect(add(1, 2)).toBe(3);
 });
+
+// 4. ceaserCipher
+
+
+// 5. analyzeArray
