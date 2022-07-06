ff0326b770e3344c3af8eb4cf406a415197284db
Derek Nunes <dereknunes28@gmail.com>
2022-07-06 12:54:34 -0500

    basic set up

---
+++ testing-practice/js-file.js
@@ -0,0 +1,28 @@
+// 1. A capitalize function that takes a string and returns it with the first character capitalized
+
+
+// 2. A reverseString function that takes a string and returns it reversed.
+
+
+/* 3. A calculator object that contains functions for the basic operations: add, subtract, divide, and multiply. 
+Each of these functions should take two numbers and return the correct calculation. */
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b, c) {
+    return a - b - c;
+}
+
+// 4. A caesarCipher function that takes a string and returns it with each character “shifted”.
+
+
+/* 5. An analyzeArray function that takes an array of numbers and returns an object with the following 
+properties: average, min, max, and length. */
+
+
+export { 
+        add,
+        subtract 
+    };
