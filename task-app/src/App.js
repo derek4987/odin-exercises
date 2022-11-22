5646ad5da021d9c36eccb5d0d2f7cba5df6b4fe4
Derek Nunes <dereknunes28@gmail.com>
2022-11-21 23:51:24 -0800

    create Overview component

--- task-app/src/App.js
+++ task-app/src/App.js
@@ -1,10 +1,11 @@
 // App.js
 import React from "react";
+import Overview from "./component/Overview";
 
 function App() {
   return (
     <div>
-
+      <Overview title="testing" />
     </div>
   );
 }
---
+++ task-app/src/component/Overview.js
@@ -0,0 +1,9 @@
+// Overview.js component
+
+import React from "react";
+
+function Overview(props) {
+    return <div>{props.title}</div>;
+}
+
+export default Overview;
