31f2521d845b6fea65a3d1cc7fdb9dfc8bb9ce44
Derek Nunes <dereknunes28@gmail.com>
2022-11-30 23:38:19 -0800

    add input and button field in App.js

--- task-app/src/App.js
+++ task-app/src/App.js
@@ -4,10 +4,20 @@ import Overview from "./component/Overview";
 
 function App() {
   return (
-    <div>
+    <>
+    <div className="tasks">
       <Overview title="testing" />
     </div>
+    <div>
+      <input type="text" placeholder="Enter task"/>
+      <button type="button">Enter</button>
+    </div>
+      </>
   );
 }
 
 export default App;
+
+/* function for button that adds button.value to task array.
+Will call <Overview task="button.value" /> and append to end of task list
+*/
