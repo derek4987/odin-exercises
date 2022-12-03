dcbf1348056f98b4bb8edf9f0612df84d9409e14
Derek Nunes <dereknunes28@gmail.com>
2022-12-02 23:23:52 -0800

    Added functionality to App.js and used Overview.js to display tasks entered in the input field

--- task-app/src/App.js
+++ task-app/src/App.js
@@ -1,23 +1,36 @@
 // App.js
-import React from "react";
+import React, { useState } from "react";
 import Overview from "./component/Overview";
 
 function App() {
-  return (
-    <>
-    <div className="tasks">
-      <Overview title="testing" />
-    </div>
-    <div>
-      <input type="text" placeholder="Enter task"/>
-      <button type="button">Enter</button>
-    </div>
-      </>
-  );
+
+	const [tasks, setTask] = useState([]);
+
+	const updateTasksList = () => {
+		const input = document.querySelector('input');
+		if (input.value !== '') {
+			const newTask = input.value;
+			input.value = '';
+			
+			// using push instead of 'concat' cause an error for some reason ??
+			setTask(tasks.concat(newTask));	
+			// using '...'
+			// setTask(current => [...current, newTask]);
+
+		} else return;		
+	}
+
+	return (
+		<>
+		<div>
+    	  <input type="text" placeholder="Enter task"/>
+    	  <button type="button" className="submitButton" onClick={updateTasksList}>Enter</button>
+    	</div>
+    	<div className="tasks">
+    	  <Overview title="Tasks list:" tasks={tasks}/>
+    	</div>
+    	</>
+	);
 }
 
 export default App;
-
-/* function for button that adds button.value to task array.
-Will call <Overview task="button.value" /> and append to end of task list
-*/
--- task-app/src/component/Overview.js
+++ task-app/src/component/Overview.js
@@ -3,7 +3,36 @@
 import React from "react";
 
 function Overview(props) {
-    return <div>{props.title}</div>;
+
+	// if I want to just enter a single function inside <ul>:
+	/*function displayTasks(props) {
+		return (
+			props.tasks.map((task, index) => {
+				return (
+					<li key={index}>{task}</li>
+				);
+			})	
+		);		
+	}*/
+
+    return (
+        <>
+        <div>{props.title}</div>
+        <ul>
+		  {props.tasks.map((task, index) => {
+			return (
+			  <li key={index}>{task}</li>
+			);
+		  })}
+        </ul>
+        </>
+    );
 }
 
 export default Overview;
+
+
+
+// google "reactjs lists and keys" for how to add entire block of list values inside list
+
+// google "reactjs using the state hook" for using state in functional component
