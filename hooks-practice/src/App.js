8d04cb84d0aa9f885fb9a595cb887a0a07b517e7
Derek Nunes <dereknunes28@gmail.com>
2023-01-05 00:08:10 -0800

    initial react set up for hooks-practice

---
+++ hooks-practice/src/App.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import './App.css';
+
+// exercise 1: useState *comment out each function to isolate the specific exercise you wish to see
+function App() {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default App;
