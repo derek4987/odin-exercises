172f13a74a1fc5be19d3114efbaeee71659583c3
Derek Nunes <dereknunes28@gmail.com>
2023-04-24 21:54:08 -0700

    react testing part 1

---
+++ react-testing/src/setupTests.js
@@ -0,0 +1,7 @@
+// testing file 
+
+import React from 'react';
+import "@testing-library/react"
+import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
+// import TestComponent from "path-to-test-component";
